75bb0071fc9cfdd09af7d0409a99e8964fd5c293
RISHABH ANANAD <93331538+mafiarishabh@users.noreply.github.com>
2022-04-03 11:11:40 +0530

    Alive Fixed

--- LUCY/modules/alive.py
+++ LUCY/modules/alive.py
@@ -9,16 +9,16 @@ from LUCY.events import register
 from LUCY import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/70657821f00fc1fb8e539.jpg"
+LUCY = "https://telegra.ph/file/70657821f00fc1fb8e539.jpg"
 
 @register(pattern=("/lucy"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm lucy bot.** \n\n"
   TEXT += "💞 **LUCY IS ALIVE** \n\n"
-  TEXT += f"💞 **Owner : {SAIZO} https://t.me/LUCY_OWNER** \n\n"
+  TEXT += f"💞 **Owner : [SAIZO](https://t.me/LUCY_OWNER)** \n\n"
   TEXT += f"💞 **Library Version :** `{telever}` \n\n"
   TEXT += f"💞 **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"💞 **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For using lucy 💞**"
   BUTTON = [[Button.url("Help", "https://t.me/LUCY_MANAGER2_bot?start=help"), Button.url("Support", "https://t.me/lucyhelp")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+  await tbot.send_file(event.chat_id, LUCY, caption=TEXT,  buttons=BUTTON)
